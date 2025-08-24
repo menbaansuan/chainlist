af12880967b700d42455089b91ac32ff761c6d47
MenBaanSuan <apisitmenz@gmail.com>
2025-08-25 05:14:51 +0700

    Create chainid-{chainid_number}.js

---
+++ constants/additionalChainRegistry/chainid-{chainid_number}.js
@@ -0,0 +1,37 @@
+{
+  "name": "Ethereum Mainnet",
+  "chain": "ETH",
+  "rpc": [https://rpc.jfinchain.com
+    "https://eth.llamarpc.com",
+    "https://rpc.ankr.com/eth",
+    "https://cloudflare-eth.com"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Ether",
+    "symbol": "ETH",
+    "decimals": 18
+  },
+  "features": [
+    { "name": "EIP155" },
+    { "name": "EIP1559" }
+  ],
+  "infoURL": "https://ethereum.org",
+  "shortName": "eth",
+  "chainId": 1,
+  "networkId": 1,
+  "icon": "https://cryptologos.cc/logos/ethereum-eth-logo.png",
+  "explorers": [
+    {
+      "name": "etherscan",
+      "url": "https://etherscan.io",
+      "icon": "etherscan",
+      "standard": "EIP3091"
+    },
+    {
+      "name": "blockchair",
+      "url": "https://blockchair.com/ethereum",
+      "standard": "EIP3091"
+    }
+  ]
+}
